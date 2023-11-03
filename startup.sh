0b11d480fabce974e8a70ea9ed1d980f1733aba9
Priyanka Sharma <66081113+pritianka@users.noreply.github.com>
2023-11-02 17:06:07 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,12 +6,6 @@ kind create cluster --config cluster.yaml
 echo "Applying the Kubernetes manifests for this demo..."
 kubectl apply -f kind/
 
-# Wait for the pod starting with "ollama" to be running
-while [[ $(kubectl get pods --no-headers | grep 'ollama' | awk '{print $3}') != "Running" ]]; do
-  echo "Waiting for the LLM pod to be running..."
-  sleep 5
-done
-
 # Wait for the pod starting with "db" to be running
 while [[ $(kubectl get pods --no-headers | grep 'database' | awk '{print $3}') != "Running" ]]; do
   echo "Waiting for the database pod to be running..."
