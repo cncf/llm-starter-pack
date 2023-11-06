8f15fbbe6b6160015bd0c297358f23916dd26c4f
Taylor Dolezal <onlydole@gmail.com>
2023-11-06 11:54:33 -0600

    Update teardown script to include ports
    Signed-off-by: Taylor Dolezal <onlydole@gmail.com>

--- teardown.sh
+++ teardown.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-echo "Applying the Kubernetes manifests for this demo..."
+echo "Removing the Kubernetes manifests from this demo..."
 kubectl delete -f kind/bot-deployment.yaml
 kubectl delete -f kind/bot-service.yaml
 kubectl delete -f kind/database-deployment.yaml
 kubectl delete -f kind/database-pvc.yaml
 kubectl delete -f kind/database-service.yaml
+
+# Find all kubectl port-forward processes and kill them
+echo "Identifying all kubectl port-forward processes..."
+
+# Use pgrep to find the PIDs of the processes with 'kubectl port-forward' command
+pids=$(pgrep -f "kubectl port-forward")
+
+if [ -z "$pids" ]; then
+    echo "No kubectl port-forward processes found."
+else
+    # Display the PIDs and kill them
+    for pid in $pids; do
+        echo "Terminating process with PID: $pid"
+        kill $pid
+        if [ $? -eq 0 ]; then
+            echo "Successfully terminated process with PID: $pid"
+        else
+            echo "Failed to terminate process with PID: $pid. You may need to run the script as root or use 'sudo'."
+        fi
+    done
+fi
+
+echo "All kubectl port-forward processes have been terminated."
